41462162ebf5b4cadc195d1f7c1a3f429cc3265b
Shehbaz <mdshehbaz666666@gmail.com>
2023-11-08 20:25:12 +0530

    connectDB_v7_chai_co

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+ 
+const connect_DB = async ()=>{
+    try {
+const connectionInstance = await mongoose.connect(`${process.env.Mongo_url}/${DB_NAME}`)
+        console.log(`Mongo db connected !! DB host ${connectionInstance.connection.host}`)
+        
+    } catch (err) {
+        console.log("MongoDB connection error FAILED ", err)
+        process.exit(1);
+    }
+}
+
+export default connect_DB;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
-const hello ="hi"
-const hello1 ="hi kya hal"
+// require('dotenv')
+// require('dotenv').config()
+import dotenv from "dotenv"
+import  connect_DB  from "./db/index.js"
+
+dotenv.config({
+        path: './env'
+    })
+
+connect_DB()
+
+
+
+// import { express } from "express";
+// const app = express();
+// (async()=>{
+//     try {
+//        await mongoose.connect(`${process.env.Mongo_url}/${DB_NAME}`);
+//         app.on(error,()=>{
+//             console.log(error);
+//             throw error
+//         });
+
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`Active port no ${process.env.PORT}`)
+//         })
+//     } catch (error) {
+//         console.error("ERROR : " ,error);
+//         throw error;
+//     }
+// })(); 
