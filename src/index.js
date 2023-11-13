7c262b929a5e324536ac6f5c6ed5cf4bcef1c2d6
Shehbaz <mdshehbaz666666@gmail.com>
2023-11-13 22:36:14 +0530

    v8_custom_api_response

--- src/index.js
+++ src/index.js
@@ -1,25 +1,37 @@
 // require('dotenv')
 // require('dotenv').config()
-import dotenv from "dotenv"
-import  connect_DB  from "./db/index.js"
+import dotenv from "dotenv";
+import connect_DB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-        path: './env'
-    })
+  path: "./env",
+});
 
 connect_DB()
+  .then(() => {
+    app.on(error,()=>{
+        console.log(error);
+        throw error
+    });
+    
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Active connection at Port 8000");
+    });
+  })
+  .catch((err) => console.log("mongoDB Connection failed ", err));
 
 
 
-// import { express } from "express";
+  // import { express } from "express";
 // const app = express();
 // (async()=>{
 //     try {
 //        await mongoose.connect(`${process.env.Mongo_url}/${DB_NAME}`);
-//         app.on(error,()=>{
-//             console.log(error);
-//             throw error
-//         });
+        // app.on(error,()=>{
+        //     console.log(error);
+        //     throw error
+        // });
 
 //         app.listen(process.env.PORT,()=>{
 //             console.log(`Active port no ${process.env.PORT}`)
@@ -28,4 +40,4 @@ connect_DB()
 //         console.error("ERROR : " ,error);
 //         throw error;
 //     }
-// })(); 
+// })();
