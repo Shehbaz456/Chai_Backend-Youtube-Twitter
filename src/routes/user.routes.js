df8d510bd959661097e464b5ed6c0098bbac8a03
Shehbaz <mdshehbaz666666@gmail.com>
2025-04-22 13:22:16 +0530

    Add user account management features: change password, get current user, update account details, and user avatar/cover image

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 const router = Router();
-import { loginUser, logoutUser, registerUser,refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserCoverImage ,updateUserAvatar} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyToken } from "../middlewares/auth.middleware.js";
 
@@ -23,5 +23,13 @@ router.post(
 router.post("/login",loginUser);
 router.post("/logout",verifyToken,logoutUser);
 router.post("/refresh-token",refreshAccessToken);
+router.post("/change-password",verifyToken,changeCurrentPassword);
+router.get("/current-user",verifyToken,getCurrentUser);
+router.patch("/update-account",verifyToken,updateAccountDetails);
+
+
+router.patch("/avatar",verifyToken,upload.single("avatar"),updateUserAvatar);
+router.patch("/cover-image",verifyToken,upload.single("coverImage"),updateUserCoverImage);
+
 
 export default router;
