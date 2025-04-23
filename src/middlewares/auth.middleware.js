8c300a4b73fe258e3da06f4a43f4bfe6d365fd6d
Shehbaz <mdshehbaz666666@gmail.com>
2025-04-23 21:22:58 +0530

    add initial route and model files for comments, dashboard, healthcheck, likes, playlists, subscriptions, tweets, and videos.

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,7 +6,7 @@ import { User } from "../models/user.model.js";
 export const verifyToken = asyncHandler(async(req,res,next)=>{
     try {
     // verify jwt token 
-    const token  =  req.cookies.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+    const token  =  req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
 
     if(!token){
         throw new ApiError(401, "Unauthorized request")
