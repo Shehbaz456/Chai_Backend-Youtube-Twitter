da2c2c0c200a3a78581eaa6288371479c72c3d37
Shehbaz <mdshehbaz666666@gmail.com>
2025-04-27 16:59:38 +0530

    Add subscription and dashboard functionality with corresponding routes and controllers

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -3,7 +3,6 @@ import {Like} from "../models/like.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
-import { User } from "../models/user.model.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
