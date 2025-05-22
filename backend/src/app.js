8650499431a75dc898d73b73ca4cf39fc54545c4
Shehbaz <mdshehbaz666666@gmail.com>
2025-05-23 00:50:54 +0530

    Fix CORS methods array formatting in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const app =express();
 
 app.use(cors({
     origin : process.env.CORS_ORIGIN,
-    methods:["GET,HEAD,PUT,PATCH,POST,DELETE"],
+    methods:["GET","HEAD","PUT","PATCH","POST","DELETE"],
     credentials:true,
 }))
 
